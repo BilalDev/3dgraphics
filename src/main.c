06078e09e0088af1b475c154062610c9ddd72550
Bilal <hello@bilalhamed.me>
2023-05-18 19:27:43 +0200

    world matrix

--- src/main.c
+++ src/main.c
@@ -30,8 +30,8 @@ void setup(void)
                                              window_width,
                                              window_height);
 
-    // load_obj_file_data("./assets/f22.obj");
-    load_cube_mesh_data();
+    load_obj_file_data("./assets/f22.obj");
+    // load_cube_mesh_data();
 }
 
 void process_input(void)
@@ -130,11 +130,16 @@ void update(void)
         for (int j = 0; j < 3; j++)
         {
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
-            transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
+            mat4_t world_matrix = mat4_identity();
+
+            // Order matters, always have to scale, rotate and then translate: [T]*[R]*[S]*v
+            world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_x, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
+            world_matrix = mat4_mul_mat4(translation_matrix, world_matrix);
+
+            transformed_vertex = mat4_mul_vec4(world_matrix, transformed_vertex);
 
             transformed_vertices[j] = transformed_vertex;
         }
